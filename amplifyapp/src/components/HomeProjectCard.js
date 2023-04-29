452e4a7aecaa54912d9434fdb8e2736b3b236907
Dan-S-Lee <34869440+Dan-S-Lee@users.noreply.github.com>
2023-04-29 18:29:44 -0500

    Removing alt image text

--- amplifyapp/src/components/HomeProjectCard.js
+++ amplifyapp/src/components/HomeProjectCard.js
@@ -6,7 +6,7 @@ function HomeProjectCard({params}) {
         <div className="projectCard">
             <a href={params.caseStudyHref} className="projectLink">
                 <img src={params.caseStudyImgRef} className="projectImage"
-                     alt="Responsive image"/>
+                     alt="Responsive Card"/>
                 <h2 className="proj-title-main">{params.projectName}</h2>
                 <p className="projDesc">{params.projectDesc}</p>
             </a>
