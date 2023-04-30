ab0f139c8bf493c371544802090d6f046dfee920
Dan-S-Lee <34869440+Dan-S-Lee@users.noreply.github.com>
2023-04-30 14:49:39 -0500

    Updating navbar

--- amplifyapp/src/components/NavBar.js
+++ amplifyapp/src/components/NavBar.js
@@ -2,19 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavBar.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from 'react-router-dom';
 
 function NavBar() {
   return (
       <>
+        <a id={"top"}></a>
           <Navbar fixed={"top"} className={"header"} expand="lg">
               <Navbar.Brand href="/">Lauren Hurr</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="ms-auto">
-                      <Nav.Link href="/">Work</Nav.Link>
-                      <Nav.Link href="/about">About</Nav.Link>
-                      <Nav.Link href="/Lauren Hurr Resume.pdf">Resume</Nav.Link>
-                      <Nav.Link href="mailto:lauren.hurr0212@gmail.com">Contact</Nav.Link>
+                      <NavLink className={({ isActive }) => "nav-link" + (isActive ? " underline" : "")} to="/" end>Work</NavLink>
+                      <NavLink className={({ isActive }) => "nav-link" + (isActive ? " underline" : "")} to="/about" end>About</NavLink>
+                      <NavLink className={"nav-link"} to="/Lauren Hurr Resume.pdf">Resume</NavLink>
+                      <NavLink className={"nav-link"} to="mailto:lauren.hurr0212@gmail.com">Contact</NavLink>
                   </Nav>
               </Navbar.Collapse>
           </Navbar>
