5e08e714683d3d5bc37033f1f5433f3681300611
Dan-S-Lee <34869440+Dan-S-Lee@users.noreply.github.com>
2023-04-30 14:46:59 -0500

    Updating references and home banner

--- amplifyapp/src/components/HomeBanner.js
+++ amplifyapp/src/components/HomeBanner.js
@@ -10,10 +10,8 @@ function HomeBanner() {
         <Row>
             <Col xl={6} md={12} className={"HomeBanner"}>
                 <Container fluid className={"introContainer"}>
-                    <p className="intro-headline">Product Designer currently working at CapTech.
-                    </p>
-                    <p className="intro-paragraph">
-                        Lauren is a data-driven and strategic product designer with 4+ years of experience.
+                    <p className="intro-headline">
+                        <b>Lauren Hurr</b> is a Product Designer currently working at CapTech.
                     </p>
                 </Container>
             </Col>
