8b3c4facc088ed4000aa0820c554ca7bb34856d6
Dan-S-Lee <34869440+Dan-S-Lee@users.noreply.github.com>
2023-05-09 22:52:51 -0500

    Updating pdf to open in new tab

--- amplifyapp/src/components/NavBar.js
+++ amplifyapp/src/components/NavBar.js
@@ -15,7 +15,7 @@ function NavBar() {
                   <Nav className="ms-auto">
                       <NavLink className={({ isActive }) => "nav-link" + (isActive ? " underline" : "")} to="/" end>Work</NavLink>
                       <NavLink className={({ isActive }) => "nav-link" + (isActive ? " underline" : "")} to="/about" end>About</NavLink>
-                      <Nav.Link className={"nav-link"} href="/lauren_hurr_resume.pdf">Resume</Nav.Link>
+                      <Nav.Link className={"nav-link"} href="/lauren_hurr_resume.pdf" target={"_blank"}>Resume</Nav.Link>
                       <Nav.Link className={"nav-link"} href="mailto:lauren.hurr0212@gmail.com">Contact</Nav.Link>
                   </Nav>
               </Navbar.Collapse>
